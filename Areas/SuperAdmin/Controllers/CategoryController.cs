34d9d65d58e9636ddb4da71fb9655824e94d5438
Yusif Nazarbayov <tu4rwlg2x@code.edu.az>
2022-06-08 07:23:26 +0400

    feat: CRUD features added

--- Areas/SuperAdmin/Controllers/CategoryController.cs
+++ Areas/SuperAdmin/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using FrontToBack.DAL;
 using FrontToBack.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace FrontToBack.Areas.SuperAdmin.Controllers
 {
@@ -46,6 +47,70 @@ namespace FrontToBack.Areas.SuperAdmin.Controllers
             _context.Categories.Remove(dbCategory);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            bool isExistName =  _context.Categories.Any(c => c.Name.ToLower() == category.Name.ToLower());
+            if (isExistName)
+            {
+                ModelState.AddModelError("Name", "The category already exist");
+                return View();
+            }
+            Category newCategory = new Category();
+            newCategory.Name = category.Name;
+            newCategory.Desc = category.Desc;
+
+            await _context.Categories.AddAsync(newCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null) return NotFound();
+            Category dbCategory = await _context.Categories.FindAsync(id);
+            if (dbCategory == null) return NotFound(); 
+            return View(dbCategory);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id, Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            Category dbCategory = await _context.Categories.FindAsync(id);
+            Category existCategoryName =
+                _context.Categories.FirstOrDefault(c => c.Name.ToLower() == category.Name.ToLower());
+
+            if (existCategoryName != null)
+            {
+                if (dbCategory != existCategoryName)
+                {
+                    ModelState.AddModelError("Name", "This name already exist");
+                    return View();
+                }
+            }
+            
+            if (dbCategory == null) return NotFound();
+            dbCategory.Name = category.Name;
+            dbCategory.Desc = category.Desc;
+            await _context.SaveChangesAsync();
+            //TempData["message"] = dbCategory.Name + "is successfully updated";
+            return RedirectToAction("Detail", "Category", new {@id=id});
         }
         
     }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FrontToBack.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "This field is required"), MaxLength(10, ErrorMessage = "Shouldn't exceed 10 characters")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "This field is required"), MaxLength(500, ErrorMessage = "Shouldn't exceed 50 characters")]
         public string Desc { get; set; } 
         public virtual IEnumerable<Product> Products { get; set; }
     }
